d639ac7befe882b3f5e5ffbfee2d5acba1a136b9
Zhiwei-Z <zhiweizhang2012@gmail.com>
2019-02-15 21:06:52 -0800

    adding vector correlation

--- utils.js
+++ utils.js
@@ -1,6 +1,15 @@
 // functions
-function correlation(v1, v2){
 
+/*
+v1, v2 are two list of numbers
+Assume v1, v2 belong to the same course subject
+*/
+function correlation(v1, v2){ 
+	sum = 0;
+	for (i = 0; i < v2.length; i ++){
+		sum += v1[i] * v2[i];
+	}
+	return sum;
 }
 
 function user_to_vector(user){
@@ -12,5 +21,5 @@ function rank_vectors_by_correlation(source, list_of_vectors){
 }
 
 function rank_users(object_user, others){
-	
+
 }
