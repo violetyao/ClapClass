d21b3298ea0c2a91ae82bce41bb27ed25090616a
Zhiwei-Z <zhiweizhang2012@gmail.com>
2019-02-15 21:14:41 -0800

    filling some functions

--- utils.js
+++ utils.js
@@ -12,10 +12,23 @@ function correlation(v1, v2){
 	return sum;
 }
 
-function rank_vectors_by_correlation(source, list_of_vectors){
-
+function matrix_correlation(m1, m2){
+	cor = 0
+	for (i = 0; i < m1.length; i ++){
+		cor += correlation[m1[]]
+	}
 }
 
-function rank_users(object_user, others){
+function add()
+
+/*
+return a list of indices of candidates, from the most correlated to the least correlated
+user_list is [[sbj_1], [subj_2], ... ]
+candidates is 	[
+					[[sbj_11], [subj_12], ... ],
+					[[sbj_21], [subj_22], ... ]
+				]
+*/
+function rank_users(user_list, candidates){
 
 }
