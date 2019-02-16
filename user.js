11d6c3c422f2a09881382831373890d081d3d29a
yuhongc <mikechen212@gmail.com>
2019-02-16 11:18:54 -0800

    user.js changes

--- user.js
+++ user.js
@@ -1,16 +1,16 @@
-class user {
+class User {
     sid;
     answers;
     classes;
     name;
     preferences;
 
-    constructor(sid, answers, classes, name, preferences) {
+    constructor(sid, answers, classes, name, preference) {
         this.sid = sid;
         this.classes = classes;
         this.answers = answers;
         this.name = name;
-        this.preferences = preferences;
+        this.preferences = preference;
         this.write();
     }
 
@@ -25,4 +25,24 @@ class user {
         })
     }
 
+}
+
+function test_user_boxing() {
+    var sid = document.getElementById("sid").value;
+    var answers_text = document.getElementById("answers").value;
+    var answers = {};
+    var answer_pairs = answers_text.split(" ");
+    answer_pairs.forEach(function (pair) {
+        pair = pair.split(":");
+        answers[pair[0]] = pair[1];
+    });
+    var classes = {};
+    var classes_text = document.getElementById("classes").value.split(";");
+    classes_text.forEach(function (_class) {
+        _class = _class.split(" ");
+        classes[_class[0]] = _class[1];
+    })
+    var name = document.getElementById("name").value;
+    var preference = {"27056890": 2};
+    User = new User(sid, answers, classes, name, preference);
 }
