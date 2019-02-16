1aec708133af308e90e789ba022727addd15620c
yuhongc <mikechen212@gmail.com>
2019-02-16 12:03:16 -0800

    fix import issues

--- user.js
+++ user.js
@@ -1,48 +1,22 @@
 class User {
-    sid;
-    answers;
-    classes;
-    name;
-    preferences;
-
-    constructor(sid, answers, classes, name, preference) {
+    constructor(sid, answers, classes, name, preference, uid) {
         this.sid = sid;
         this.classes = classes;
         this.answers = answers;
         this.name = name;
         this.preferences = preference;
+        this.uid = uid;
         this.write();
     }
 
     write() {
-        firebase.database().ref("AllUsers").set({
-            sid: {
-                "Answer": this.answers,
-                "Class": this.classes,
-                "Name": this.name,
-                "Preference": this.preferences
-            }
+        firebase.database().ref("AllUsers/" + this.sid).set({
+            "Answer": this.answers,
+            "Class": this.classes,
+            "Name": this.name,
+            "Preference": this.preferences,
+            "UID": this.uid
         })
     }
 
-}
-
-function test_user_boxing() {
-    var sid = document.getElementById("sid").value;
-    var answers_text = document.getElementById("answers").value;
-    var answers = {};
-    var answer_pairs = answers_text.split(" ");
-    answer_pairs.forEach(function (pair) {
-        pair = pair.split(":");
-        answers[pair[0]] = pair[1];
-    });
-    var classes = {};
-    var classes_text = document.getElementById("classes").value.split(";");
-    classes_text.forEach(function (_class) {
-        _class = _class.split(" ");
-        classes[_class[0]] = _class[1];
-    })
-    var name = document.getElementById("name").value;
-    var preference = {"27056890": 2};
-    User = new User(sid, answers, classes, name, preference);
 }
