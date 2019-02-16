aeb48b65858447bc8268ce8c59b8940574bcbbd0
Zhiwei-Z <zhiweizhang2012@gmail.com>
2019-02-15 20:51:51 -0800

    adding basic functions

--- utils.js
+++ utils.js
@@ -1,1 +1,16 @@
 // functions
+function correlation(v1, v2){
+
+}
+
+function user_to_vector(user){
+
+}
+
+function rank_vectors_by_correlation(source, list_of_vectors){
+
+}
+
+function rank_users(object_user, others){
+	
+}
