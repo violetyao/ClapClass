5045a6b6ba70dc0c132464ac8e385ff95e62fe01
Ziyao Zhang <ziyaointl@gmail.com>
2019-02-15 23:10:05 -0800

    Crawl all pages

--- crawler/main.py
+++ crawler/main.py
@@ -6,10 +6,17 @@ def construct_url(page):
     return "https://classes.berkeley.edu/search/class?page=" + str(page) + \
            "&f%5B0%5D=im_field_term_name%3A831"
 
-result = requests.get(construct_url(0))
-soup = BeautifulSoup(result.content)
-i = 0
-for div in soup.find_all("div", class_=re.compile("handlebarData")):
-    print(div['data-json'])
-    print(i)
-    i += 1
+f = open('data', 'a')
+
+for i in range(379):
+    result = requests.get(construct_url(i))
+    soup = BeautifulSoup(result.content)
+    for div in soup.find_all("div", class_=re.compile("handlebarData")):
+        f.write(div['data-json'])
+        f.write("\n")
+    if result.status_code == 200:
+        print("Page {0} success!".format(i))
+    else:
+        raise RuntimeError("Page {0} failed!", i)
+
+f.close()
