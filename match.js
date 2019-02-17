b208afa72c051a824620036ab2a0f83ab25a73d2
Boyuan Chen <buoyuan99@gmail.com>
2019-02-17 03:18:42 -0800

    organize folder

--- match.js
+++
@@ -1,9 +0,0 @@
-function button_get_first_match(){
-    let user_id = fetch_user_id();
-    console.log("userid: " + user_id);
-    console.log("get_userid_list:");
-    console.log(get_userid_list());
-    let first_match = rank_users(user_id, get_userid_list());
-    console.log("getting the first match ... ");
-    console.log(first_match);
-}
