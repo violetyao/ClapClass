fe03379ac0d5c6cb98290dc7f88d1fa261889800
yuhongc <mikechen212@gmail.com>
2019-02-17 06:52:42 -0800

    fix login

--- Website_Front/js/login.js
+++ Website_Front/js/login.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
         $("#loginform").css("visibility", "hidden");
         $("#loginform").animate({left: "12%"}, 400);
         $("#signupform").animate({left: "20%"}, 400);
-        $("#signupform").animate({left: "15%"}, 400);
+        $("#signupform").animate({left: "30%"}, 400);
         $("#signupform").css("visibility", "visible");
     });
 
@@ -14,8 +14,8 @@ $(document).ready(function () {
         $("#main").animate({left: "70%"}, 500);
         $("#signupform").css("visibility", "hidden");
         $("#signupform").animate({left: "75%"}, 400);
-        $("#loginform").animate({left: "60%"}, 400);
-        $("#loginform").animate({left: "55%"}, 400);
+        $("#loginform").animate({left: "65%"}, 400);
+        $("#loginform").animate({left: "70%"}, 400);
         $("#loginform").css("visibility", "visible");
     });
 
